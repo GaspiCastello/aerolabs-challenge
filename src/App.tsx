1c89fad6c7b50913b4afadea6b5ddfc7f4580dfb
Gaspar <gasparcastello@gmail.com>
2022-04-30 22:03:12 -0300

    adding chakra cart and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable react/function-component-definition */
-import React,{ useEffect,useState } from 'react';
-import Pr from './App copy';
+import React, { useEffect, useState } from 'react'
+import Pr from './App copy'
 
-const App: React.FC = () => (<Pr />)
+const App: React.FC = () => <Pr />
 
-   export default App
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { ThemeProvider } from '@chakra-ui/core'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <React.StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+)
