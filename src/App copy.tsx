f1ffdba71db63114d78b3fccca4328e05ac3c470
Gaspar <gasparcastello@gmail.com>
2022-04-30 21:32:40 -0300

    fetch products ok

---
+++ src/App copy.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable react/function-component-definition */
+import React,{ useEffect,useState } from 'react';
+
+// eslint-disable-next-line max-len
+const token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjY5OGJjN2FkYzQ1NTAwMWE2Y2JjM2IiLCJpYXQiOjE2NTEwODQyMzF9.z98IN81adDVPCXeVSZHGWBYxlY7tnQeWKQgT_YfkiYA"
+
+const Pr: React.FC = () => { 
+   const [res,setRes]=useState<object[]>([])
+   console.log("res in app:",res)
+   useEffect(()=>{
+      const headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Accept', 'application/json');
+      headers.append('Authorization', `${token}`);
+      fetch('https://coding-challenge-api.aerolab.co/products', {
+         method: 'GET',
+         headers,
+      })
+      .then(response => response.json())
+      .then((products) => {setRes(products)})
+      .catch((error) => {console.error(error);
+      });
+   },[])
+    return(<div/>)
+}
+
+   export default Pr
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable react/function-component-definition */
-import React from 'react';
-import logo from "./logo.svg"
-import './App.css'
+import React,{ useEffect,useState } from 'react';
+import Pr from './App copy';
 
-const App: React.FC = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
-    </div>)
-export default App
+const App: React.FC = () => (<Pr />)
+
+   export default App
