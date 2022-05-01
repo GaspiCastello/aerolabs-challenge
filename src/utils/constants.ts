bfec8f59f02e3b4b64d481a705d4771554489fa5
Gaspar <gasparcastello@gmail.com>
2022-05-01 08:18:26 -0300

    components added

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const information = {
+    avatar: '//placehold.it/128x128',
+    banner: '//placehold.it/1440x320',
+    title: 'Fidelity Program',
+    description: 'Exchanges your points  !',
+    color: 'green',
+    phone: '+543426156014',
+    social: [
+        {
+            name: 'instagram',
+            link: '',
+        },
+        {
+            name: 'facebook',
+            link: '',
+        },
+        {
+            name: 'twitter',
+            link: '',
+        },
+    ],
+}
